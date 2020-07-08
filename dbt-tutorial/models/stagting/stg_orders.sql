f33f9bc3f6303edec860b37fb231c6cbfad64ec9
cheikhetvagha <c.elvagha@elvagha-consulting.com>
2020-07-08 03:17:42 +0200

    added table scripts

---
+++ dbt-tutorial/models/stagting/stg_orders.sql
@@ -0,0 +1,17 @@
+with source AS 
+(
+    SELECT * from raw_orders
+)
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from source
+
+)
+
+select * from renamed
